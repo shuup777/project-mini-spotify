8cb4cb7a27dc0e81928943f43efe2abdb1625dc5
shuup777 <pashuuphehey@gmail.com>
2025-11-20 23:38:32 +0700

    Update login support for artist_app and templates

--- mini_spotify/urls.py
+++ mini_spotify/urls.py
@@ -35,5 +35,7 @@ urlpatterns = [
     path('finance/', include('finance_app.urls')),
 
     # login
-    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
+    path( 'accounts/login/',
+    auth_views.LoginView.as_view(template_name='login.html'),
+    name='login')
 ]
