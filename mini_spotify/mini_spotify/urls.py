9c6b1718eb74562b3ff2211f1add7762186cf9b2
lidyaazalia91 <3lidyaazalia@gmail.com>
2025-11-21 21:47:18 +0700

    Update urls.py

--- mini_spotify/mini_spotify/urls.py
+++ mini_spotify/mini_spotify/urls.py
@@ -14,9 +14,28 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import sys
+import os
+
+project_path = r"C:\Users\Lenovo\Documents\project-mini-spotify"
+if project_path not in sys.path:
+    sys.path.insert(0, project_path)
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from user_app import views as user_views  
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    path('', user_views.home, name='home'),  
     path('admin/', admin.site.urls),
+    path('system/', include('system_admin_app.urls')),
+    path('user/', include('user_app.urls')),
+    path('artist/', include('artist_app.urls')),
+    path('finance/', include('finance_app.urls')),
+
+    # login
+    path( 'accounts/login/',
+    auth_views.LoginView.as_view(template_name='login.html'),
+    name='login')
 ]
